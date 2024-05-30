91e3078ea546b9dd89bfa844e630d173bb5eff3d
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-30 15:06:01 +0200

    Creato array di oggetti: listaCoseDaFare: con due propietà("contenuto", "fatto"). Creata struttura html ul e un li e assegnato un v-for con gli item della lista.

--- js/script.js
+++ js/script.js
@@ -3,10 +3,31 @@ const { createApp } = Vue
 createApp({
 data() {
     return {
-        message: 'Hello Vue!'
+        listaCoseDaFare: [
+            {
+                contenuto: "Fare spesa",
+                fatto: false,
+            },
+            {
+                contenuto: "Anadare dal dentista",
+                fatto: false,
+            },
+            {
+                contenuto: "Andare al mare",
+                fatto: false,
+            },
+            {
+                contenuto: "Fare Esercizio ToDoList",
+                fatto: false,
+            },
+            {
+                contenuto: "Incontrare Sara",
+                fatto: false,
+            },
+        ]
         }
     },
     methods:{
-        
+
     }
 }).mount('#app')
